f596a23df980af4379f2d0e1174720412fc180a3
Utkub24 <utkubicer24@gmail.com>
2024-11-02 18:33:25 +0300

    hw1: deleted all my previous code :)

---
+++ hw1-code/src/main.cpp
@@ -0,0 +1,6 @@
+#include "tweet.h"
+
+int main() 
+{
+
+}
---
+++ hw1-code/src/utilities.cpp
@@ -0,0 +1,11 @@
+#include "tweet.h"
+
+std::vector<Tweet> readTweetsFromFile(const std::string& filename) 
+{
+
+}
+
+void writeTweetsToFile(const std::string& filename, const std::vector<Tweet>& tweets) 
+{
+
+}
